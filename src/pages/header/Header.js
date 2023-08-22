f7e8d6adaacaa7f673c7fed1344b15c8833876d1
Lanre Adeolu <ladeolu2016@gmail.com>
2023-08-22 14:07:03 +0100

    changing home page

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import Typewriter from "typewriter-effect";
+// import Typewriter from "typewriter-effect";
+import Typewriter from "typewriter-effect/dist/core";
 import "../header/Header.css";
 import "../header/HMediaQueries.css";
 import { Link } from "react-router-dom";
@@ -10,6 +11,49 @@ const Header = () => {
   // Scroll To top
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+
+    const app = document.getElementById("home-header");
+    const typewriter = new Typewriter(app, {
+      loop: true,
+      delay: 150,
+      deleteSpeed: 70,
+    });
+    typewriter
+      .pauseFor(1000)
+      .typeString("JavaScript.")
+      .pauseFor(700)
+      .deleteChars(11)
+      .typeString("Python.")
+      .pauseFor(700)
+      .deleteChars(7)
+      .typeString("ReactJS.")
+      .pauseFor(700)
+      .deleteChars(3)
+      .typeString(" Native.")
+      .pauseFor(700)
+      .deleteChars(13)
+      .typeString("NodeJS.")
+      .pauseFor(700)
+      .deleteChars(7)
+      .typeString("ExpressJS.")
+      .pauseFor(700)
+      .deleteChars(10)
+      .typeString("Figma.")
+      .pauseFor(700)
+      .deleteChars(6)
+      .typeString("Design.")
+      .pauseFor(700)
+      .deleteChars(6)
+      .typeString("ata.")
+      .pauseFor(900)
+      .deleteChars(5)
+      .typeString("Power BI.")
+      .pauseFor(700)
+      .deleteChars(9)
+      .typeString("Animation.")
+      .pauseFor(700)
+      .deleteChars(10)
+      .start();
   }, []);
 
   return (
@@ -18,7 +62,8 @@ const Header = () => {
       <header>
         <Navbar />
 
-        <div className="head pt-5">
+        {/* Mobile */}
+        <div className="head">
           <div className="head-content text-white container">
             <div className="mobile-header mb-4">
               <h1 className="display-4 inner-text fw-bold">
@@ -28,27 +73,15 @@ const Header = () => {
               </h1>
             </div>
 
-            <div className="typewriter-effect display-1 mb-4 desktop-header">
-              <h1 className="typewritter-fixed">Want to acquire skills in</h1>
-              <Typewriter
-                options={{
-                  strings: [
-                    "Web Development?",
-                    "Web Design?",
-                    "Product Design?",
-                    "Data Science?",
-                    "Data Analysis?",
-                    "Switching Careers?",
-                    // "Looking to hire?",
-                  ],
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
+            {/* Desktop */}
+            <div className="typewriter-effect mb-4 desktop-header">
+              <h1 className="display-1 typewriter-fixed">
+                Learn <span className="typewriter-text" id="home-header"></span>
+              </h1>
             </div>
-            <h1 className="h1-1 mb-3 desktop-subtitle">
-              You've come to the right place!
-            </h1>
+            <h4 className=" mb-3 desktop-subtitle fst-italic">
+              The straight bus to your destination!
+            </h4>
             <h6 className="mb-3 mobile-subtitle">
               Acquire, develop and sharpen your tech skills with custom-made
               schedule and curriculum tailored just for you to set you apart
@@ -56,7 +89,7 @@ const Header = () => {
             </h6>
 
             <Link to="/programmes">
-              <button className="btn btn-lg btn-getstarted px-5 mt-3">
+              <button className="btn btn-lg btn-getstarted ">
                 <span>Get started</span>
               </button>
             </Link>
